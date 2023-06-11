2b2d00aa09f16597a8991c494f7f8e99aeabb861
React4Ever <latifalex95@gmail.com>
2023-06-11 23:13:27 +0200

    updating

--- netchill/src/App.js
+++ netchill/src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import './App.css';
-
+import Row from './Row';
+import requests from './requests';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to NetChill !</h1>
       </header>
+        <Row title={'NETFLIX ORIGINALS'} fetchUrl={requests.fetchNetflixOriginals}/>
+        <Row title={'Trending now'} fetchUrl={requests.fetchTrending}/>
     </div>
   );
 }

ca6aecc813fe5839394d14947207d50b443e6c0f
React4Ever <latifalex95@gmail.com>
2023-06-12 20:32:22 +0200

    updating rows

--- netchill/src/App.js
+++ netchill/src/App.js
@@ -6,10 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to NetChill !</h1>
+        <h1>C'est pas encore fini ...</h1>
       </header>
-        <Row title={'NETFLIX ORIGINALS'} fetchUrl={requests.fetchNetflixOriginals}/>
+        <Row title={'NETFLIX ORIGINALS'} fetchUrl={requests.fetchNetflixOriginals} isLargerRow/>
         <Row title={'Trending now'} fetchUrl={requests.fetchTrending}/>
+        <Row title={'Top Rated'} fetchUrl={requests.fetchTopRated}/>
+        <Row title={'Action Movies'} fetchUrl={requests.fetchActionMovies}/>
+        <Row title={'Comedy Movies'} fetchUrl={requests.fetchComedyMovies}/>
+        <Row title={'Horror Movies'} fetchUrl={requests.fetchHorrorMovies}/>
+        <Row title={'Romance Movies'} fetchUrl={requests.fetchRomanceMovies}/>
+        <Row title={'Documentaries'} fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- netchill/src/Row.js
+++ netchill/src/Row.js
@@ -3,7 +3,7 @@ import axios from './axios';
 import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
-function Row({title, fetchUrl}){
+function Row({title, fetchUrl, isLargerRow}){
     const [movies, setMovies] = useState([]);
     //run a piece of code in a specific condition -> mettre les images au moment du chargement
     useEffect(()=>{ // agit que au chargement de la page
@@ -21,7 +21,7 @@ function Row({title, fetchUrl}){
             <div className="row_posters">
                 {}
                 {movies.map(movie =>(
-                    <img key={movie.id} className="row_poster"  src={base_url+movie.poster_path} alt={movie.name}/>
+                    <img key={movie.id} className={`row_poster ${isLargerRow && "row_poster_large"}`} src={base_url+ (isLargerRow ? movie.poster_path : movie.backdrop_path)} alt={movie.name}/>
                 ))}
             </div>
         </div>
